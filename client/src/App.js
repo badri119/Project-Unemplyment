aea53d38c0ffac1aa912bfd7c81380890c357c58
AjishKalia99 <ajishkalia123@gmail.com>
2024-04-11 13:58:05 -0400

    Login Page React and Barebones Login API

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Main from "./pages/tic-tac-toe/Main";
 import Why from "./pages/why/Why";
 import LandingPage from "./LandingPage";
+import Login from "./pages/login/login";
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 
@@ -13,6 +14,7 @@ const App = () => {
         <Route exact path="/" element={<LandingPage />} />
         <Route exact path="/tic-tac-toe" element={<Main />} />
         <Route path="/why" element={<Why />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ client/src/pages/login/login.js
@@ -0,0 +1,19 @@
+import React from "react"; 
+import "./login.css";
+
+const Login = () => {
+    return (
+        <div class="login-container">
+            <div class="login-form-container">
+                <div class="login-element"><h3>Username</h3></div>
+                <div class="login-element"><input type="text" name="username" class="login-form-input" /></div>
+                <div class="login-element"><h3>Password</h3></div>
+                <div class="login-element"><input type="password" name="password" class="login-form-input" /></div>
+                <div class="login-element buttons"><input type="submit" class="get-started"/></div>
+            </div>
+            <h3>Don't have an account? <br/><br/> <a href="/register" class="link">Create an Account</a> instead!</h3>
+        </div>
+    );
+};
+
+export default Login;
