0c1b03b6770cd596a39de381135eb1b2eda376db
AjishKalia99 <ajishkalia123@gmail.com>
2024-04-12 15:24:18 -0400

    Merge Commit

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,15 +1,28 @@
-import React from "react"; 
+import React , { useState } from "react"; 
 import "./login.css";
 
 const Login = () => {
+    const [data, setData] = useState(null);
+    function APILogin(){
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET','localhost:8080/Login')
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                setData(JSON.parse(xhr.responseText));
+            }
+        };
+        xhr.send();
+    }
     return (
         <div class="login-container">
             <div class="login-form-container">
+            <form>
                 <div class="login-element"><h3>Username</h3></div>
                 <div class="login-element"><input type="text" name="username" class="login-form-input" /></div>
                 <div class="login-element"><h3>Password</h3></div>
                 <div class="login-element"><input type="password" name="password" class="login-form-input" /></div>
                 <div class="login-element buttons"><input type="submit" class="get-started"/></div>
+            </form>
             </div>
             <h3>Don't have an account? <br/><a href="/register" class="link">Create an Account</a> instead!</h3>
         </div>
